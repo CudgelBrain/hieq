83199c24f98bea6746c8ebf5318cca321a25f7ff
workplace36 <79572450+workplace36@users.noreply.github.com>
2022-12-15 00:04:56 +0530

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from 'react';
-import { isEmpty } from 'lodash';
+import { isEmpty } from 'lodash'; 
 import { Router, Switch } from 'react-router-dom';
 
 // Imported from project
